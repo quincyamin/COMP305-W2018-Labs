432ea3753ad0202dd7e877f017293ac0342efc90
quincyamin <quincyamin@users.noreply.github.com>
2018-02-04 19:49:32 -0500

    Created lab2

---
+++ COMP305-W2018/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+
+    public Sprite mySprite;
+    public GameObject character;
+
+    private SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = character.GetComponent<SpriteRenderer>();
+	}
+
+    void OnMouseDown()
+    {
+        rend.sprite = mySprite;
+    }
+
+
+}
